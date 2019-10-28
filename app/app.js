2b23f7122c09a9a3845dae397602dfc188e5a9c6
Pawan Sharma <pawan@uxgorilla.com>
2019-10-28 23:26:43 +0530

    authentication middleware added and file upload working

--- app/app.js
+++ app/app.js
@@ -7,13 +7,16 @@ const app = express();
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
-
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "*");
     res.setHeader("Access-Control-Allow-Methods", "*");
     next();
 });
+
+// app.use((req, res, next) => {
+//     console.log(req.headers);
+// });
 
 app.use('/api/users', users);
 app.use('/api/videos', videos);
--- routes/videos.js
+++ routes/videos.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const path = require('path');
+const formidable = require('formidable');
 const router = express.Router();
 const fs = require('fs');
+const checkIfAuthenticated = require('../middlewares/checkIfAuthenticated')
 
 router.get('/', (req, res) => {
     const path = './assets/sample.mp4';
@@ -25,15 +28,43 @@ router.get('/', (req, res) => {
         };
 
         res.writeHead(206, head);
-        file.pipe(res)
+        file.pipe(res);
     } else {
         const head = {
             'Content-Length': fileSize,
             'Content-Type': 'video/mp4',
         };
         res.writeHead(200, head);
-        fs.createReadStream(path).pipe(res)
+        fs.createReadStream(path).pipe(res);
     }
+});
+
+router.post('/new', checkIfAuthenticated, (req, res) => {
+    console.log(req.user);
+    new formidable.IncomingForm().parse(req)
+        .on('fileBegin', (name, file) => {
+            file.path = path.join(__dirname, '../assets/', file.name);
+        })
+        .on('field', (name, field) => {
+            console.log('Field', name, field);
+        })
+        .on('file', (name, file) => {
+            // console.log('Uploaded file', name, file);
+
+        })
+        .on('aborted', () => {
+            console.error('Request aborted by the user');
+        })
+        .on('error', (err) => {
+            console.error('Error', err);
+            throw err;
+        })
+        .on('end', () => {
+            res.json({
+                result: true,
+                message: 'File Uploaded Successfully'
+            });
+        })
 });
 
 module.exports = router;
